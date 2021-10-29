52797641d830d713a7e41a67bfff4393beb89bce
Michalczyk <dawid.michalczyk@capgemini.com>
2021-10-29 09:11:48 +0200

    Changed logic in classes, registering, creating account and login

--- accounts.py
+++ accounts.py
@@ -13,17 +13,23 @@ class Register():
         Register.accountsBasePlaceholder['password'] = self.password
         Register.accountsBasePlaceholder['email'] = self.email
 
-    @classmethod
-    def saveAccountInDB(cls):
+    def saveAccountInDB():
         askForUserName = input("Please provide your userName: \n")
         askForPassword = input("Please provide you password: \n")
         askForEmail = input("Please provide your email: \n")
+        playerDetails = CreatePlayer.instantiate_player_to_csv()
 
         Register(
             userName = askForUserName,
             password = askForPassword,
-            email = askForEmail
+            email = askForEmail,
         )
+        Register.accountsBasePlaceholder['playerName'] = playerDetails['playerName']
+        Register.accountsBasePlaceholder['playerLevel'] = playerDetails['playerLevel']
+        Register.accountsBasePlaceholder['playerAlive'] = playerDetails['playerAlive']
+        Register.accountsBasePlaceholder['playerExperience'] = playerDetails['playerExperience']
+        Register.accountsBasePlaceholder['playerHealth'] = playerDetails['playerHealth']
+
         with open ('accounts.csv', 'a+') as accountsFile:
             accountsFile.read()
             for value in Register.accountsBasePlaceholder.values():
@@ -50,7 +56,7 @@ class LogIntoTheGame(): #Register ?
         print("Logged in succesfully")
         return loggedInUser
 
-class CreatePlayer(Register):
+class CreatePlayer():
     writeToFilePlayerPlaceholder = {}
     def __init__(self, playerName = str, playerLevel = 1, playerAlive = True, playerExperience = 0, playerHealth=100):
         
@@ -58,12 +64,13 @@ class CreatePlayer(Register):
         self.playerLevel = playerLevel
         self.playerAlive = playerAlive
         self.playerExperience = playerExperience
-        self.playerHelth = playerHealth
+        self.playerHealth = playerHealth
 
         CreatePlayer.writeToFilePlayerPlaceholder["playerName"] = self.playerName
         CreatePlayer.writeToFilePlayerPlaceholder["playerLevel"] = self.playerLevel
         CreatePlayer.writeToFilePlayerPlaceholder["playerAlive"] = self.playerAlive
         CreatePlayer.writeToFilePlayerPlaceholder["playerExperience"] = self.playerExperience
+        CreatePlayer.writeToFilePlayerPlaceholder["playerHealth"] = self.playerHealth
         
     @classmethod
     def instantiate_player_to_csv(cls):
@@ -71,42 +78,4 @@ class CreatePlayer(Register):
         CreatePlayer(
             playerName = askForPlayerName,
         )
-        with open ('players.csv', 'a+') as playersFile:
-            playersFile.read()
-            for value in CreatePlayer.writeToFilePlayerPlaceholder.values():
-                playersFile.write(str(value))
-                playersFile.write(",")
-            playersFile.write("\n")
-        CreatePlayer.writeToFilePlayerPlaceholder.clear()
-
-    #dont know how to assign player to account, need to rethink that
-    def assignPlayerToAccount():
-        playerName = CreatePlayer.playerName
-
-        with open ('accounts.csv', 'a+') as accountsFile:
-            accountsFile.read()
-            reader = csv.reader(accountsFile, delimiter=' ', quotechar='|')
-            for line in reader:
-                for value in line:
-                    if(playerName in value):
-                        searchUserName = True
-            if searchUserName==True:
-                loggedInUser = LogIntoTheGame.getLoginData()
-                with open ('accounts.csv', 'a+') as accountsFile:
-                    accountsFile.read()
-                    reader = csv.reader(accountsFile, delimiter=' ', quotechar='|')
-                    for line in reader:
-                        for value in line:
-                            if(loggedInUser in value):
-                                continue
-                            if(value==None):
-                                accountsFile.write(",")
-                                accountsFile.write(str(playerName))
-                        break
-                    
-
-#check if logged in
-# LogIntoTheGame.getLoginData()
-Register.saveAccountInDB()
-CreatePlayer.instantiate_player_to_csv()
-CreatePlayer.assignPlayerToAccount()
+        return CreatePlayer.writeToFilePlayerPlaceholder
--- startGame.py
+++ startGame.py
@@ -1,2 +1,7 @@
+from accounts import LogIntoTheGame, Register
+
 class StartMenu:
     print("Welcome in the text adventure Game!\n")
+
+LogIntoTheGame.getLoginData()
+# Register.saveAccountInDB()
